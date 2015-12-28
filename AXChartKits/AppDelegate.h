981c8e4d8239d86d660215d9eb9360a8094422ca
aiXing <862099730@qq.com>
2015-12-28 16:19:07 +0800

    Initial Commit

---
+++ AXChartKits/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXChartKits
+//
+//  Created by ai on 15/12/28.
+//  Copyright © 2015年 AiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
